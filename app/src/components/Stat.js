5a7f01425a2af4e46ce3431fae9faa760eb6fdc2
Draelnir <lollier.guillaume@gmail.com>
2019-04-24 16:23:55 +0200

    Fetch data working, displaying them as a json works also

--- app/src/components/Stat.js
+++ app/src/components/Stat.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjU0MiwiaWRlbiI6IjIxMTEyODAyNTQxMDk2MTQxMCIsIm1kIjp7fSwidHMiOjE1NTYwMjQ1Njc2MDl9.a1Dd43GVTcwJhuhtyqIHH5jKPNaIBBnlsriA40d5xjk'
+let fetchedData 
 
-class Stat extends Component {
-  render() {
-    return (
-      <div>
-        {"Stat.js"}
-      </div>
-      )
-  }
+
+class Stat extends Component {	
+	
+
+componentDidMount(){
+	fetch('https://api.royaleapi.com/player/QGUPYVY9,P9VCCCJJ2', {
+		headers:{
+			'auth' : token
+		}})
+	.then(result =>{
+		return result.json()
+	})
+	.then(data =>{	
+		fetchedData = data
+		console.log(fetchedData)
+	})
+}
+	
+render() {
+	return (
+		<div>
+		{"Stat.js"}
+		<div><pre>{JSON.stringify(fetchedData, null, 2) }</pre></div>
+		</div>
+	)
+}
+	
+	
 }
 
 const mapStateToProps = (state) => {
-  return state
+	return state
 }
 
 export default connect(mapStateToProps)(Stat)
