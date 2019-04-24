8947955854cdf95aa8f77315f1f97bd392f4e369
axeldonadio <donadio.axel@gmail.com>
2019-04-24 16:34:15 +0200

    apexchart

--- app/src/components/vues/AddPlayerTagCard.js
+++ app/src/components/vues/AddPlayerTagCard.js
@@ -6,7 +6,7 @@ class AddPlayerTagCard extends Component {
     return (
       <div className="tagCard">
         {this.props.player.key !== 1 ? <div className="vs">VS</div> : null}
-        <input type="text" value={this.props.player.nameTag} onChange={e => this.props.onChangeHandler({value: e.target.value, id: this.props.player.key})} name={"player" + this.props.player.key} maxLength="9"/>
+        <input type="text" value={this.props.player.nameTag} onChange={e => this.props.onChangeHandler({value: e.target.value, id: this.props.player.key})} name={"player" + this.props.player.key} placeholder="#NAMETAG" maxLength="9"/>
         {this.props.player.key !== 1 ? <button onClick={() => this.props.removePlayer(this.props.player.key)}>X</button> : null}
       </div>
     )
