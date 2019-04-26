cf8f7eb9d4eb84e90a935efafe00b3338fb263f4
Draelnir <lollier.guillaume@gmail.com>
2019-04-26 17:47:58 +0200

    Header footer on stat page

--- app/src/components/Stat.js
+++ app/src/components/Stat.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Chart from "react-apexcharts";
 import PieChart from './vues/PieChart'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/Stat.css';
+import Header from './vues/Header'
+import Footer from './vues/Footer'
 
 class Stat extends Component {
 render() {
 	return (
 		<div>
+			<Header />
 			<PieChart players={this.props.players}/>
 			{"Stat.js"}
 			<div><pre>{JSON.stringify(this.props.players, null, 2) }</pre></div>
+			<Footer />
 		</div>
 	)
 }
